8288ac97eab226f475b3a7715b1d24494c0c408e
fmonfasani <fmonfasani@gmail.com>
2024-09-10 13:15:28 -0300

    fix the scaffolding

--- src/auth/user.interface.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-export interface User {
-  id: number;
-  username: string;
-  email: string;
-}
--- src/logistic/logistic.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('logistic')
-export class LogisticController {}
